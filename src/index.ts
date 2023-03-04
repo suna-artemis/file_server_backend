71c6623a98e1ae7e6c6f2878059c2c38eca676e7
suna <suan.artemis@gmail.com>
2023-03-05 06:52:51 +0900

    feat✨:  modify listening port

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createYoga } from 'graphql-yoga'
+import { createYoga, useLogger } from 'graphql-yoga'
 import { createServer } from 'http'
 import { execute, parse } from 'graphql'
 
@@ -10,14 +10,19 @@ import { createContext } from './context'
  */
 const main = async () => {
   // create yoga middleware
-  const yoki = createYoga({ schema, context: createContext })
+  const yoki = createYoga({
+    schema,
+    graphqlEndpoint: '/api/v1/',
+    context: createContext,
+    multipart: false,
+  })
 
   // create server include yoga
   const server = createServer(yoki)
 
   // listen on port 4000 and waiting some requests
-  server.listen(4000, () => {
-    console.info('Server is running on http://localhost:4000/graphql')
+  server.listen(10000, () => {
+    console.info('Server is running on http://localhost:10000/api/v1/')
   })
 }
 
