c40a33d8c0db1ef8666d871d59d0a6023cf242c3
suna <suan.artemis@gmail.com>
2023-03-06 02:00:13 +0900

    perf👌:  optimizing directory detail fetching

--- README.md
+++ README.md
@@ -6,55 +6,55 @@
 
 1. install package
 
-    `pnpm install`
+   `pnpm install`
 
 2. start dev
 
-    `pnpm dev`
+   `pnpm dev`
 
 3. start production
 
-    `pnpm start`
-
+   `pnpm start`
 
 ## backup
+
 1. install prisma
 
-​	`pnpm add -D --save-exact prisma @prisma/client`
+​ `pnpm add -D --save-exact prisma @prisma/client`
 
 2. init prisma
 
-    `pnpm prisma init`
+   `pnpm prisma init`
 
 3. create table structure
 
-    ```prisma
-    datasource db {
-        provider = "sqlite"
-        url      = "file:./dev.db"
-    }
-    
-    generator client {
-        provider = "prisma-client-js"
-    }
-    
-    model Link {
-        id          Int      @id @default(autoincrement())
-        createdAt   DateTime @default(now())
-        desc        String
-        url         String
-    }
-    ```
+   ```prisma
+   datasource db {
+       provider = "sqlite"
+       url      = "file:./dev.db"
+   }
+
+   generator client {
+       provider = "prisma-client-js"
+   }
+
+   model Link {
+       id          Int      @id @default(autoincrement())
+       createdAt   DateTime @default(now())
+       desc        String
+       url         String
+   }
+   ```
 
 4. execute migrate command and named it like 'init'
 
-    `pnpm prisma migrate dev`
+   `pnpm prisma migrate dev`
 
 5. update prisma model from db
 
-    <code>pnpm prisma generate</code>
+   <code>pnpm prisma generate</code>
 
 6. running prisma GUI
-    `pnpm prisma studio`
+   `pnpm prisma studio`
 
-7. 
+7. https://nodejs.org/api/fs.html
--- src/schema.ts
+++ src/schema.ts
@@ -3,128 +3,75 @@ import { Link, Comment } from '@prisma/client'
 import { GraphQLContext } from './context'
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime'
 import { GraphQLError } from 'graphql'
+import * as fsp from 'fs/promises'
+
+const STATIC_ROOT_PATH = './static'
+
+enum FileInfoType {
+  FILE = 'file',
+  DIRECTORY = 'directory',
+  // like socket, link, fifo pipe,
+  OTHER = 'other',
+}
+
+interface FileInfo {
+  name: string
+  type: FileInfoType
+}
 
 const typeDefs = /* GraphQL */ `
-  type Link {
-    id: ID!
-    desc: String!
-    url: String!
-    comments: [Comment!]
+  scalar File
+  type FileInfo {
+    name: String!
+    type: String!
   }
-
-  type Comment {
-    id: ID!
-    body: String!
-    linkId: ID!
-    link: Link
-  }
-
   type Query {
     info: String!
-    links: [Link!]!
-    link(id: ID!): Link!
-    comments: [Comment!]!
-    getCommentsByLinkId(linkId: ID!): [Comment!]
+    FileInfo: FileInfo!
+    parentDirectory(parentDirectory: String!): String!
+    getFileListByParentDirectory(parentDirectory: String!): [FileInfo!]!
   }
-  scalar File
   type Mutation {
-    postLink(desc: String!, url: String!): Link!
-    postCommentOnLink(linkId: ID!, body: String!): Comment!
     uploadFileList(fileList: [File!]!): String!
   }
 `
-// declare scalar File to allow upload file
-
 const resolvers = {
-  Link: {
-    id: ({ id }: Link) => id,
-    desc: ({ desc }: Link) => desc,
-    url: ({ url }: Link) => url,
-  },
-  Comment: {
-    id: ({ id }: Comment) => id,
-    body: ({ body }: Comment) => body,
-    linkId: ({ linkId }: Comment) => linkId,
-  },
   Query: {
     info: () => 'This is an amazing framework!',
-    links: (parent: unknown, {}: any, { prisma }: GraphQLContext) => {
-      return prisma.link.findMany()
-    },
-    comments: (parent: unknown, {}: any, { prisma }: GraphQLContext) => {
-      return prisma.comment.findMany()
-    },
-    link: (
-      parent: unknown,
-      { id }: { id: string },
-      { prisma }: GraphQLContext
-    ) => {
-      return prisma.link.findUniqueOrThrow({
-        where: {
-          id: parseInt(id),
-        },
+    getFileListByParentDirectory: async (
+      _: unknown,
+      { parentDirectory }: { parentDirectory: string }
+    ): Promise<FileInfo[]> => {
+      const res = await fsp.readdir(`${STATIC_ROOT_PATH}${parentDirectory}`, {
+        withFileTypes: true,
       })
+      return res.map((file) => ({
+        name: file.name,
+        type: file.isFile()
+          ? FileInfoType.FILE
+          : file.isDirectory()
+          ? FileInfoType.DIRECTORY
+          : FileInfoType.OTHER,
+      }))
     },
-    getCommentsByLinkId: (
-      parent: unknown,
-      { linkId }: any,
-      { prisma }: GraphQLContext
-    ) => {
-      return prisma.comment.findMany({
-        where: {
-          linkId: parseInt(linkId),
-          // skip count of record pages
-          // skip,
-          // take count of records
-          // take
-        },
-      })
+    parentDirectory: (
+      _: unknown,
+      { parentDirectory }: { parentDirectory: string }
+    ) => parentDirectory,
+    FileInfo: {
+      name: ({ name }: FileInfo) => name,
+      type: ({ type }: FileInfo) => type,
     },
   },
   Mutation: {
-    postLink: async (
-      parent: unknown,
-      { desc, url }: { desc: string; url: string },
-      { prisma }: GraphQLContext
+    uploadFileList: async (
+      _: unknown,
+      {
+        parentDirectory,
+        fileList,
+      }: { parentDirectory: string; fileList: File[] }
     ) => {
-      const newLink = await prisma.link.create({
-        data: {
-          url,
-          desc,
-        },
-      })
-      return newLink
-    },
-    postCommentOnLink: async (
-      parent: unknown,
-      { linkId, body }: { linkId: string; body: string },
-      { prisma }: GraphQLContext
-    ) => {
-      const comment = await prisma.comment
-        .create({
-          data: {
-            linkId: parseInt(linkId),
-            body,
-          },
-        })
-        // error handle
-        .catch((err: unknown) => {
-          if (
-            err instanceof PrismaClientKnownRequestError &&
-            // @see https://www.prisma.io/docs/reference/api-reference/error-reference#p2003
-            err.code === 'P2003'
-          ) {
-            return Promise.reject(
-              new GraphQLError(
-                `Cannot post comment on non-existing link with id ${linkId}`
-              )
-            )
-          }
-        })
-      return comment
-    },
-    uploadFileList: async (_: unknown, { fileList }: { fileList: File[] }) => {
-      console.log('fileList :>> ', fileList)
+      // fsp.appendFile()
       return 'fileList'
     },
   },
